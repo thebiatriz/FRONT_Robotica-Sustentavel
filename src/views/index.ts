4b8333e2b93606ce0fa2d247fbdbd0beacbf67b4
Beatriz Monteiro <biafigmonteiro@gmail.com>
2025-05-11 22:09:13 -0300

    (chore) Rebase with main

--- src/views/index.ts
+++ src/views/index.ts
@@ -30,4 +30,28 @@ export class Views {
     static Team(): Promise<any> {
         return import("./Team/team.vue");
     }
+
+    static Computer(): Promise<any> {
+        return import("./Computer/computer.vue");
+    }
+
+    static ComputerForm(): Promise<any> {
+        return import("./Computer/computer-form.vue");
+    }
+
+    static Donation(): Promise<any> {
+        return import("./Donation/donation.vue");
+    }
+
+    static Sale(): Promise<any> {
+        return import("./Sale/sale.vue");
+    }
+
+    static ComputerOutput(): Promise<any> {
+        return import("./ComputerOutput/computer-output.vue");
+    }
+
+    static NotFound(): Promise<any> {
+        return import("./NotFound/not-found.vue");
+    }
 }
