3ef695b5738da525ec6918189f2299c5499082b9
Beatriz Monteiro <biafigmonteiro@gmail.com>
2025-05-11 22:09:13 -0300

    add font roboto and adjust navbar

--- src/components/Navbar/navbar.vue
+++ src/components/Navbar/navbar.vue
@@ -1,6 +1,11 @@
 <template>
     <nav class="fixed top-0 w-full z-50">
-        <Menubar :model="items" class="flex justify-between h-20 items-center !px-[5vw] md:!px-[2vw] !rounded-none">
+        <Menubar :model="items"
+            class="border !border-[#DDDDDD] flex justify-between h-20 items-center !px-[5vw] md:!px-[2vw] !rounded-none"
+            :pt="{
+                itemLabel: ['text-[#666666] font-semibold'],
+                submenuIcon: ['text-[#666666]']
+            }">
             <template #start>
                 <div @click="navigateTo('/')" class="flex items-center ml-4 cursor-pointer">
                     <img src="../../assets/images/robotics-logo.svg" alt="logo" class="h-10" />
@@ -60,4 +65,3 @@ export default defineComponent({
     }
 })
 </script>
-
